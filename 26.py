ae16a9053640e8ece258db857234139a7c915370
Juan Morales <js.moralesc@uniandes.edu.co>
2018-05-10 07:33:51 -0500

    v1.0

--- 26.py
+++ 26.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-rand = np.random.random(1000)*10.0+35.0
+rand = np.random.random(100000)*10.0+35.0
+error = 5.0
+ds = np.array([61,115,31,177])
 
+plt.hist(rand)
+plt.show()
 
